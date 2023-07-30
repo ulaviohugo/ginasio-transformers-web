4002744f144e4d56466c59707e4806b67ed9542e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-30 18:35:17 +0100

    feat: Add add employ repository method

--- app/business/infra/db/prisma/employee-prisma-repository.ts
+++ app/business/infra/db/prisma/employee-prisma-repository.ts
@@ -1,13 +1,21 @@
 import {
   LoadEmployeeRepositoryResult,
   LoadEmployeeRepository,
+  AddEmployeeRepository,
 } from '@/app/business/data/protocols'
 import { PrismaService } from '.'
+import { Employee } from '@/app/business/domain/models'
 
-export class EmployeePrismaRepository implements LoadEmployeeRepository {
+export class EmployeePrismaRepository
+  implements LoadEmployeeRepository, AddEmployeeRepository
+{
   private prisma: PrismaService
   constructor() {
     this.prisma = new PrismaService()
+  }
+
+  async add(param: Employee): Promise<Employee> {
+    return (await this.prisma.employee.create({ data: param })) as Employee
   }
 
   async loadAll(): Promise<LoadEmployeeRepositoryResult> {
