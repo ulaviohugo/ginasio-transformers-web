4fe803570185ac29fb81e7b342ec9cb6010e17e2
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-31 21:57:58 +0100

    feat: Add validation on DeleteEmployeeController

--- app/business/main/factories/controllers/delete-employee-controller-factory.ts
+++ app/business/main/factories/controllers/delete-employee-controller-factory.ts
@@ -1,7 +1,11 @@
 import { DeleteEmployeeController } from '@/app/business/presentation/controllers'
 import { Controller } from '@/app/business/presentation/protocols'
 import { makeDeleteEmployee } from '..'
+import { GreaterThanValidation } from '@/app/business/validation/validators'
 
 export const makeDeleteEmployeeController = (): Controller => {
-  return new DeleteEmployeeController(makeDeleteEmployee())
+  return new DeleteEmployeeController(
+    makeDeleteEmployee(),
+    new GreaterThanValidation('id', 0)
+  )
 }
--- app/business/presentation/controllers/delete-employee-controller.ts
+++ app/business/presentation/controllers/delete-employee-controller.ts
@@ -1,13 +1,23 @@
 import { DeleteEmployee } from '../../domain/usecases'
-import { ok, serverError } from '../helper'
-import { Controller, HttpResponse } from '../protocols'
+import { badRequest, notFound, ok, serverError } from '../helper'
+import { Controller, HttpResponse, Validation } from '../protocols'
 
 export class DeleteEmployeeController implements Controller {
-  constructor(private readonly deleteEmployee: DeleteEmployee) {}
+  constructor(
+    private readonly deleteEmployee: DeleteEmployee,
+    private readonly validation: Validation
+  ) {}
 
   async handle({ id }: DeleteEmployeeControllerRequest): Promise<HttpResponse> {
     try {
-      await this.deleteEmployee.delete(Number(id))
+      const error = this.validation.validate({ id })
+      if (error) {
+        return badRequest(error)
+      }
+      const result = await this.deleteEmployee.delete(Number(id))
+      if (result === null) {
+        return notFound()
+      }
       return ok({ message: 'Registo excluído com sucesso.' })
     } catch (error) {
       return serverError(error)
