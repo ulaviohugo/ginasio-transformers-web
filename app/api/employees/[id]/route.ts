26b75ee59ad33366216889e0ab8d7e6f4f2fc301
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-31 23:15:39 +0100

    refactor: Change route order

--- app/api/employees/[id]/route.ts
+++ app/api/employees/[id]/route.ts
@@ -10,12 +10,12 @@ type Params = {
   }
 }
 
-export async function DELETE(request: Request, { params }: Params) {
-  const newReq = { ...request, id: params.id }
-  return adaptRoute(makeDeleteEmployeeController(), newReq)
-}
-
 export async function PUT(request: Request, { params }: Params) {
   ;(request as any).id = params.id
   return adaptRoute(makeUpdateEmployeeController(), request)
+}
+
+export async function DELETE(request: Request, { params }: Params) {
+  const newReq = { ...request, id: params.id }
+  return adaptRoute(makeDeleteEmployeeController(), newReq)
 }
