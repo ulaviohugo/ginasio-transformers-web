dcf46cf31cbda741eb4fb10ebbe4bc16532a4e3d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-31 23:14:27 +0100

    feat: Add UpdateEmployee route

--- app/api/employees/[id]/route.ts
+++ app/api/employees/[id]/route.ts
@@ -1,5 +1,8 @@
 import { adaptRoute } from '@/app/business/main/adapters'
-import { makeDeleteEmployeeController } from '@/app/business/main/factories'
+import {
+  makeDeleteEmployeeController,
+  makeUpdateEmployeeController,
+} from '@/app/business/main/factories'
 
 type Params = {
   params: {
@@ -10,4 +13,9 @@ type Params = {
 export async function DELETE(request: Request, { params }: Params) {
   const newReq = { ...request, id: params.id }
   return adaptRoute(makeDeleteEmployeeController(), newReq)
+}
+
+export async function PUT(request: Request, { params }: Params) {
+  ;(request as any).id = params.id
+  return adaptRoute(makeUpdateEmployeeController(), request)
 }
