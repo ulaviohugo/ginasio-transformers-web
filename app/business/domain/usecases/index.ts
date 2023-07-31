c87a676f27dc65d2eb1e884a1b2a3f7a63bb8f49
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-31 23:07:11 +0100

    feat: Add UpdateEmployee usecase interface

--- app/business/domain/usecases/index.ts
+++ app/business/domain/usecases/index.ts
@@ -1,3 +1,4 @@
 export * from './add-employee'
 export * from './delete-employee'
 export * from './load-employees'
+export * from './update-employee'
---
+++ app/business/domain/usecases/update-employee.ts
@@ -0,0 +1,5 @@
+import { Employee } from '../models'
+
+export interface UpdateEmployee {
+  update(param: Employee): Promise<Employee | 'notFound' | 'emailInUse'>
+}
