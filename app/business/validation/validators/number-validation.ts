25699e4c7ce8160a9708db5e72c9f67f3427cc27
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-07-31 23:05:07 +0100

    fix: Add digit range

--- app/business/validation/validators/number-validation.ts
+++ app/business/validation/validators/number-validation.ts
@@ -4,7 +4,7 @@ export class NumberValidation implements Validation {
   constructor(private readonly fieldName: string) {}
 
   validate(input: any): Error | any {
-    const numberRegex = /^[0-9]$/
+    const numberRegex = /^[0-9]{1,50}$/
     const isValid = numberRegex.test(input[this.fieldName])
     if (!isValid) {
       return new Error(
