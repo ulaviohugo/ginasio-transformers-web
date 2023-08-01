1fb1f23b8d1f7f1be2201be49a4aed9c06fb3dec
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-01 14:44:37 +0100

    style: Show active menu item

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,25 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function Header() {
+  const path = usePathname()
   return (
     <header className="bg-gray-600 text-gray-200 h-screen">
       <nav className="flex flex-col h-full">
         <ul className="flex flex-col">
-          <Item link={'/'} text="Dashboard" />
-          <Item link={'/comercial'} text="Comercial" />
-          <Item link={'/rh'} text="RH" />
-          <Item link={'/admin'} text="Administrativo" />
+          <Item active={path == '/'} link={'/'} text="Dashboard" />
+          <Item
+            active={path.indexOf('/comercial') >= 0}
+            link={'/comercial'}
+            text="Comercial"
+          />
+          <Item active={path.indexOf('/rh') >= 0} link={'/rh'} text="RH" />
+          <Item
+            active={path == '/admin'}
+            link={'/admin'}
+            text="Administrativo"
+          />
         </ul>
         <div className="flex flex-col mt-auto p-2">
           <button className="flex justify-center border px-2 py-1 text-sm font-semibold rounded-md">
@@ -23,11 +34,17 @@ export function Header() {
 type ItemProps = {
   text: string
   link: string
+  active: boolean
 }
-const Item = ({ link, text }: ItemProps) => {
+const Item = ({ active, link, text }: ItemProps) => {
   return (
     <li>
-      <Link href={link} className="flex px-2 py-1">
+      <Link
+        href={link}
+        className={`flex px-2 py-1 ${
+          active && 'bg-gray-50 text-gray-500 rounded-l-xl'
+        }`}
+      >
         {text}
       </Link>
     </li>
