e9ff2887fbc5d4a7f1b525e64bad78e2fe2d53af
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-01 12:30:38 +0100

    feat: Add layout component

---
+++ app/components/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+import { Footer, Header } from '.'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex">
+      <Header />
+      <div className="flex-1 flex flex-col p-4 gap-4 bg-gray-50">
+        <main className="flex-1 flex ">{children}</main>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export function LayoutBody({ children }: LayoutProps) {
+  return (
+    <div className="flex-1 bg-white shadow-lg rounded-xl p-4">{children}</div>
+  )
+}
