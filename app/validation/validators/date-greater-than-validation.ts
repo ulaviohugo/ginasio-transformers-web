07652a8952f63aacf0ebe42878e8a0d3e6f705da
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-07 20:37:06 +0100

    feat: Add date gt validation

---
+++ app/validation/validators/date-greater-than-validation.ts
@@ -0,0 +1,22 @@
+import { Validation } from '@/app/infra/http/protocols'
+import { DateUtils } from '@/app/utils'
+
+export class DateGreaterThanValidation implements Validation {
+	constructor(
+		private readonly fieldName1: string,
+		private readonly fieldName2: string
+	) {}
+
+	validate(input: any): Error | any {
+		const date1: Date = DateUtils.convertToDate(input[this.fieldName1])
+		const date2: Date = DateUtils.convertToDate(input[this.fieldName2])
+
+		if (!date1 || !date2) return
+
+		if (date1 <= date2) {
+			return new Error(
+				`O parâmetro ${this.fieldName1} tem de ser maior que ${this.fieldName2}`
+			)
+		}
+	}
+}
