9f549affec47883c2acb803717eab674a443d69a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-07 20:40:38 +0100

    refactor: Use renamed validation

--- app/main/factories/controllers/delete-employee-controller-factory.ts
+++ app/main/factories/controllers/delete-employee-controller-factory.ts
@@ -1,11 +1,11 @@
 import { DeleteEmployeeController } from '@/app/infra/http/controllers'
 import { Controller } from '@/app/infra/http/protocols'
 import { makeDeleteEmployee } from '..'
-import { GreaterThanValidation } from '@/app/validation/validators'
+import { NumberGreaterThanValidation } from '@/app/validation/validators'
 
 export const makeDeleteEmployeeController = (): Controller => {
 	return new DeleteEmployeeController(
 		makeDeleteEmployee(),
-		new GreaterThanValidation('id', 0)
+		new NumberGreaterThanValidation('id', 0)
 	)
 }
