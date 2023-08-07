1d33587c527461c913131d727ee6c03d0c734369
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-07 20:40:16 +0100

    feat: Add getDatePt

--- app/utils/date-utils.ts
+++ app/utils/date-utils.ts
@@ -2,10 +2,21 @@ export class DateUtils {
 	static getDate(dateParam: Date | string, separator = '-') {
 		if (!dateParam) return null
 		const date = this.convertToDate(dateParam)
+
 		const year = date.getUTCFullYear(),
 			month = (date.getUTCMonth() + 1).toString().padStart(2, '0'),
 			day = date.getUTCDate().toString().padStart(2, '0')
 		return `${year}${separator}${month}${separator}${day}`
+	}
+
+	static getDatePt(dateParam: Date | string, separator = '-') {
+		if (!dateParam) return null
+		const date = this.convertToDate(dateParam)
+
+		const year = date.getUTCFullYear(),
+			month = (date.getUTCMonth() + 1).toString().padStart(2, '0'),
+			day = date.getUTCDate().toString().padStart(2, '0')
+		return `${day}${separator}${month}${separator}${year}`
 	}
 
 	static convertToDate(date: Date | string): Date {
