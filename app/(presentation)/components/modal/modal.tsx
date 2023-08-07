fdb2e61c48bfc66014cd365b76ad78d50be59eab
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-07 22:02:12 +0100

    style: Change close button size

--- app/(presentation)/components/modal/modal.tsx
+++ app/(presentation)/components/modal/modal.tsx
@@ -21,7 +21,10 @@ export function Modal({ children, onClose, show }: ModalProps) {
 		<div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col justify-center items-center bg-black bg-opacity-50">
 			<div className="relative flex flex-col bg-white p-5 rounded-lg max-h-[90%] max-w-5xl">
 				<button className="absolute right-2 top-2 group" onClick={handleClose}>
-					<IconClose className="group-hover:scale-150 transition-all duration-100 ease-in" />
+					<IconClose
+						size={24}
+						className="group-hover:scale-150 transition-all duration-100 ease-in"
+					/>
 				</button>
 				{children}
 			</div>
