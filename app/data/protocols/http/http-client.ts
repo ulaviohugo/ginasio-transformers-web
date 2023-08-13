e73abe12835c174a784b32838b51d8128e1a582f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-13 16:37:04 +0100

    refactor: move protocols to it´s own file

--- app/data/protocols/http/http-client.ts
+++ app/data/protocols/http/http-client.ts
@@ -1,29 +1,5 @@
+import { HttpRequestParams, HttpResponse } from '.'
+
 export interface HttpClient<T = any, R = any> {
 	request: (params: HttpRequestParams<T>) => Promise<HttpResponse<R>>
-}
-
-export type HeaderParams = HeadersInit & { 'Content-Type': 'application/json' }
-
-export type HttpRequestParams<T = any> = {
-	url: string
-	method: 'post' | 'get' | 'put' | 'delete'
-	body?: T
-	headers?: HeaderParams
-	params?: object
-}
-
-export type HttpResponse<T = any> = {
-	statusCode: HttpStatusCode
-	body?: T
-	headers?: object
-}
-
-export enum HttpStatusCode {
-	ok = 200,
-	okCreated = 201,
-	noContent = 204,
-	badRequest = 400,
-	unauthorized = 401,
-	notFound = 404,
-	serverError = 500
 }
--- app/data/protocols/http/index.ts
+++ app/data/protocols/http/index.ts
@@ -1,1 +1,28 @@
 export * from './http-client'
+
+export type HeaderParams = HeadersInit & { 'Content-Type': 'application/json' }
+
+export type HttpRequestParams<T = any> = {
+	url: string
+	method: 'post' | 'get' | 'put' | 'delete'
+	body?: T
+	headers?: HeaderParams
+	params?: object
+}
+
+export type HttpResponse<T = any> = {
+	statusCode: HttpStatusCode
+	body?: T
+	headers?: object
+}
+
+export enum HttpStatusCode {
+	ok = 200,
+	okCreated = 201,
+	noContent = 204,
+	badRequest = 400,
+	unauthorized = 401,
+	forbidden = 403,
+	notFound = 404,
+	serverError = 500
+}
