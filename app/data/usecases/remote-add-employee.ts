34b4184a19c508bd1bdf84a83abdfc498be5c586
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-13 15:02:27 +0100

    refactor: Remove unnecessary codde

--- app/data/usecases/remote-add-employee.ts
+++ app/data/usecases/remote-add-employee.ts
@@ -2,7 +2,6 @@ import { Employee } from '@/app/domain/models'
 import { AddEmployee } from '@/app/domain/usecases'
 import { HttpClient, HttpStatusCode } from '../protocols/http'
 import { UnexpectedError } from '@/app/infra/http/errors'
-import { ObjectUtils } from '@/app/utils'
 
 export class RemoteAddEmployee implements AddEmployee {
 	constructor(
@@ -11,17 +10,10 @@ export class RemoteAddEmployee implements AddEmployee {
 	) {}
 
 	async add(param: Employee): Promise<Employee> {
-		const body = ObjectUtils.removeProps<Employee>(param, [
-			'createdAt',
-			'createdBy',
-			'updatedAt',
-			'updatedBy',
-			'id'
-		])
 		const httpResponse = await this.httpClient.request({
 			method: 'post',
 			url: this.url,
-			body
+			body: param
 		})
 		switch (httpResponse.statusCode) {
 			case HttpStatusCode.ok:
