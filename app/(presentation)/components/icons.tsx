07337bf81390550f6ceeb4351469c697053a644b
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-14 13:17:29 +0100

    refactor: Remove unused icons

--- app/(presentation)/components/icons.tsx
+++ app/(presentation)/components/icons.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { IconBaseProps } from 'react-icons'
 import {
 	HiBriefcase,
-	HiCheckCircle,
 	HiHome,
-	HiInformationCircle,
 	HiKey,
 	HiMail,
 	HiPhone,
@@ -13,25 +11,21 @@ import {
 	HiTrash,
 	HiPencilAlt
 } from 'react-icons/hi'
-import { RiExternalLinkLine } from 'react-icons/ri'
-import { MdLocationOn } from 'react-icons/md'
-import { AiOutlineBuild, AiOutlineClockCircle } from 'react-icons/ai'
+import { AiOutlineClockCircle } from 'react-icons/ai'
 import { ImSpinner2 } from 'react-icons/im'
-import { IoIosMenu, IoMdClose } from 'react-icons/io'
-import { BiSearch } from 'react-icons/bi'
+import { IoMdClose } from 'react-icons/io'
+import { BiSearch, BiCategoryAlt } from 'react-icons/bi'
 
 export const IconService = (props: IconBaseProps): React.ReactElement => (
 	<HiBriefcase {...props} />
 )
+export const IconCategory = (props: IconBaseProps): React.ReactElement => (
+	<BiCategoryAlt {...props} />
+)
 export const IconClock = (props: IconBaseProps): React.ReactElement => (
 	<AiOutlineClockCircle {...props} />
 )
-export const IconCheckCircle = (props: IconBaseProps): React.ReactElement => (
-	<HiCheckCircle {...props} />
-)
-export const IconContact = (props: IconBaseProps): React.ReactElement => (
-	<HiPhone {...props} />
-)
+
 export const IconKey = (props: IconBaseProps): React.ReactElement => <HiKey {...props} />
 export const IconEmail = (props: IconBaseProps): React.ReactElement => (
 	<HiMail {...props} />
@@ -39,15 +33,7 @@ export const IconEmail = (props: IconBaseProps): React.ReactElement => (
 export const IconHome = (props: IconBaseProps): React.ReactElement => (
 	<HiHome {...props} />
 )
-export const IconInfo = (props: IconBaseProps): React.ReactElement => (
-	<HiInformationCircle {...props} />
-)
-export const IconLink = (props: IconBaseProps): React.ReactElement => (
-	<RiExternalLinkLine {...props} />
-)
-export const IconLocation = (props: IconBaseProps): React.ReactElement => (
-	<MdLocationOn {...props} />
-)
+
 export const IconPhone = (props: IconBaseProps): React.ReactElement => (
 	<HiPhone {...props} />
 )
@@ -66,6 +52,4 @@ export const IconSearch = (props: IconBaseProps): React.ReactElement => (
 
 export const IconEdit = (props: IconBaseProps) => <HiPencilAlt {...props} />
 export const IconTrash = (props: IconBaseProps) => <HiTrash {...props} />
-export const IconMenu = (props: IconBaseProps) => <IoIosMenu {...props} />
 export const IconClose = (props: IconBaseProps) => <IoMdClose {...props} />
-export const IconBuilding = (props: IconBaseProps) => <AiOutlineBuild {...props} />
