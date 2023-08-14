b3c50d82c00c89b6176b92e8e01bce70ea5d49ab
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-14 16:09:55 +0100

    refactor: Remove unnecessary dependency

--- app/data/usecases/category/db-add-category.ts
+++ app/data/usecases/category/db-add-category.ts
@@ -2,11 +2,10 @@ import { AddCategory } from '@/app/domain/usecases'
 import { CategoryRepository } from '../../protocols'
 import { Category } from '../../../domain/models'
 import { ObjectUtils } from '@/app/utils'
-import { Uploader } from '../../protocols/services'
 
 export class DbAddCategory implements AddCategory {
 	constructor(private readonly categoryRepository: CategoryRepository) {}
-	async add(param: Category, uploader?: Uploader): Promise<Category> {
+	async add(param: Category): Promise<Category> {
 		const data = ObjectUtils.trimValues(param)
 
 		const exists = await this.categoryRepository.findByName(data.name)
