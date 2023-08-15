45303c5b463b6b29aeccd15c7a010c072ff3570f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-15 15:17:37 +0100

    refactor: Add optional uploader interface

--- app/domain/usecases/product/add-product.ts
+++ app/domain/usecases/product/add-product.ts
@@ -2,5 +2,5 @@ import { Uploader } from '@/app/data/protocols/services'
 import { Product } from '../../models'
 
 export interface AddProduct {
-	add(params: Product, uploader: Uploader): Promise<Product>
+	add(params: Product, uploader?: Uploader): Promise<Product>
 }
