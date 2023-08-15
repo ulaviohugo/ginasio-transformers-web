381f1816c479a997b7bac51754b974a51e6bf73a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-15 11:59:42 +0100

    feat: Add useRedux hook

---
+++ app/(presentation)/hooks/use-redux.tsx
@@ -0,0 +1,8 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../redux'
+
+export const useRedux = () => useSelector((state: RootState) => state)
+
+export const useCategories = () => useRedux().categories.categories
+export const useEmployees = () => useRedux().employees.employees
+export const useLocations = () => useRedux().locations
