454bbd3b607eb32505ec5ac70fb51bda03850003
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-19 22:55:29 +0100

    chore: Add sale migration

---
+++ prisma/migrations/20230819215312_remove_sale_photo_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `photo` on the `tb_sales` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `tb_sales` DROP COLUMN `photo`;
