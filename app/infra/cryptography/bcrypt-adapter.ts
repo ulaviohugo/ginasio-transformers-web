1fe192ca3b2c758c169089dce86c78a1bb449cbb
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-21 15:55:59 +0100

    feat: Implement Hasher

---
+++ app/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt'
+import { Hasher } from '@/app/data/protocols/cryptography'
+
+export class BcryptAdapter implements Hasher {
+	constructor(private readonly salt = 12) {}
+
+	async hash(plaintext: string): Promise<string> {
+		return bcrypt.hash(plaintext, this.salt)
+	}
+
+	async compare(plaintext: string, digest: string): Promise<boolean> {
+		return bcrypt.compare(plaintext, digest)
+	}
+}
