5bf39cf30e9783cadbbb0273b37371d8063e26e8
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-21 15:22:46 +0100

    feat: Define authentication interface

---
+++ app/domain/usecases/auth/authentication.ts
@@ -0,0 +1,17 @@
+import { Account } from '../../models'
+
+export interface Authentication {
+	auth(params: AuthenticationParams): Promise<AuthenticationResult>
+}
+
+export type AuthenticationParams = {
+	email: string
+	password: string
+}
+
+export type AuthenticationResult =
+	| Account
+	| 'canNotLogin'
+	| 'notFound'
+	| 'invalidCredential'
+	| null
