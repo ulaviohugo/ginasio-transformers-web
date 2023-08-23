4a6c7bb787c42f7809ac2d69aa6608039d3189f4
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-23 14:39:11 +0100

    feat: Add generic on controller handle param

--- app/infra/http/protocols/controller.ts
+++ app/infra/http/protocols/controller.ts
@@ -1,7 +1,7 @@
 import { HttpResponse } from '@/app/data/protocols/http'
 
 export interface Controller<T = any> {
-	handle(request: ControllerParam<T>): Promise<HttpResponse>
+	handle(request: ControllerParams<T>): Promise<HttpResponse>
 }
 
-export type ControllerParam<T> = T & { accountId: number }
+export type ControllerParams<T> = T & { accountId: number }
