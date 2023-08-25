ac56f7112fda56453e7e4d7b1224945a8c732138
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-25 15:01:59 +0100

    chore: Ignore migrations

--- prisma/migrations/20230823215455_add_tb_sipplier_products_key/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[supplierId,category_id,product_id]` on the table `tb_supplier_products` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `tb_supplier_products_supplierId_category_id_product_id_key` ON `tb_supplier_products`(`supplierId`, `category_id`, `product_id`);
