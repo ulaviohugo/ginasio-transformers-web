015e258084e7691e8484616526edd872c2b918cf
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-08-25 14:53:41 +0100

    feat: Add PaymentUtils

---
+++ app/utils/payment-utils.ts
@@ -0,0 +1,12 @@
+export class PaymentUtils {
+	static getMethods() {
+		return [
+			'Depósito bancário',
+			'Dinheiro',
+			'Levantamento sem cartão',
+			'Multicaixa',
+			'TPA',
+			'Transferência Express'
+		]
+	}
+}
