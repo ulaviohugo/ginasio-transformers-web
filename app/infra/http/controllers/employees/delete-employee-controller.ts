a9eca39b0c14c504c93430f706e9f610444dac2d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-09-04 20:23:32 +0100

    refactor: Prevent to delete self account

--- app/infra/http/controllers/employees/delete-employee-controller.ts
+++ app/infra/http/controllers/employees/delete-employee-controller.ts
@@ -1,6 +1,6 @@
 import { DeleteEmployee } from '@/app/domain/usecases'
 import { badRequest, notFound, ok, serverError } from '../../helper'
-import { Controller, Validation } from '../../protocols'
+import { Controller, ControllerParams, Validation } from '../../protocols'
 import { HttpResponse } from '@/app/data/protocols/http'
 import { UploadService } from '@/app/services'
 import { dbErrorHandler } from '@/app/infra/db'
@@ -11,12 +11,16 @@ export class DeleteEmployeeController implements Controller {
 		private readonly validation: Validation
 	) {}
 
-	async handle({ id }: DeleteEmployeeControllerRequest): Promise<HttpResponse> {
+	async handle({ id, accountId }: ControllerParams<Params>): Promise<HttpResponse> {
 		try {
 			const error = this.validation.validate({ id })
 			if (error) {
 				return badRequest(error)
 			}
+			if (id == accountId) {
+				return badRequest(new Error('Não pode excluir a própria conta.'))
+			}
+
 			const result = await this.deleteEmployee.delete(Number(id), new UploadService())
 			if (result === null) {
 				return notFound()
@@ -28,6 +32,6 @@ export class DeleteEmployeeController implements Controller {
 	}
 }
 
-type DeleteEmployeeControllerRequest = {
+type Params = {
 	id: number
 }
