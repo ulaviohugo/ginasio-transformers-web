13441df15f79d3bd05fc3ec7f1091921648a1467
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-06 17:24:58 +0100

    feat: Add HttpResponse helper

---
+++ app/Helpers/HttpResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+class HttpResponse
+{
+	static function error($message, $status = HttpStatusCode::BAD_REQUEST)
+	{
+		return response()->json(['message' => $message], $status);
+	}
+
+	static function success($data = null, string $message = null,  $status = HttpStatusCode::OK)
+	{
+		return response()->json(
+			$data ? $data : ($message ? ['message' => $message] : null),
+			$status
+		);
+	}
+}
