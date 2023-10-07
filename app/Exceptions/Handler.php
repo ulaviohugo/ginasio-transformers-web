d493d9ae6dd7b4603c41b895f53016d5c563b258
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-07 23:24:25 +0100

    feat: Handle NotFoundHttpException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,29 +2,41 @@
 
 namespace App\Exceptions;
 
+use App\Helpers\HttpResponse;
+use App\Helpers\HttpStatusCode;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
-    ];
+	/**
+	 * The list of the inputs that are never flashed to the session on validation exceptions.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $dontFlash = [
+		'current_password',
+		'password',
+		'password_confirmation',
+	];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
-    public function register(): void
-    {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
-    }
+	/**
+	 * Register the exception handling callbacks for the application.
+	 */
+	public function register(): void
+	{
+		$this->reportable(function (Throwable $e) {
+			//
+		});
+
+		$this->renderable(function (Throwable $e, Request $request) {
+			if ($e instanceof NotFoundHttpException) {
+				if ($request->is('api/*')) {
+					return HttpResponse::error(message: 'Registo não encontrado.', status: HttpStatusCode::NOT_FOUND);
+				}
+			}
+		});
+	}
 }
