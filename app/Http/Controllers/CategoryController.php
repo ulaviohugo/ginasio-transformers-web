6e6fcfbfb947bebe3597e5cae9e497503a019555
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-07 23:25:57 +0100

    feat: Implement all api category method

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,42 +3,76 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\HttpResponse;
+use App\Http\Requests\CategoryCreateRequest;
+use App\Http\Requests\CategoryUpdateRequest;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
 
 class CategoryController extends Controller
 {
-
 	public function index()
 	{
-		return Category::all();
+		try {
+			$this->authorize('viewAny', Category::class);
+			return Category::all();
+		} catch (\Throwable $th) {
+			if ($th instanceof AuthorizationException) {
+				return HttpResponse::error(message: 'Não tem permissão para ceder este recurso');
+			}
+			return HttpResponse::error(message: 'Erro ao consultar categoria' . $th->getMessage());
+		}
 	}
 
-
-	public function store(Request $request)
+	public function store(CategoryCreateRequest $request)
 	{
-		//
+		try {
+			$createdCategory = Category::create([
+				'name' => trim($request->name),
+				'user_id' => User::currentUserId()
+			]);
+			return HttpResponse::success(data: $createdCategory);
+		} catch (\Throwable $th) {
+			return HttpResponse::error(message: 'Erro ao cadastrar categoria' . $th->getMessage());
+		}
 	}
 
-
-	public function show(string $id)
+	public function update(CategoryUpdateRequest $request, Category $category)
 	{
-		//
-	}
-
-
-	public function update(Request $request, string $id)
-	{
-		//
+		try {
+			$category->name = trim($request->name);
+			$category->user_id_update = User::currentUserId();
+			$category->save();
+			return HttpResponse::success(data: $category);
+		} catch (\Throwable $th) {
+			return HttpResponse::error(message: 'Erro ao actualizar categoria' . $th->getMessage());
+		}
 	}
 
 	public function count()
 	{
-		return HttpResponse::success(data: Category::count());
+		try {
+			$this->authorize('viewAny', Category::class);
+			return HttpResponse::success(data: Category::count());
+		} catch (\Throwable $th) {
+			if ($th instanceof AuthorizationException) {
+				return HttpResponse::error(message: 'Não tem permissão para ceder este recurso');
+			}
+			return HttpResponse::error(message: 'Erro ao consultar categoria');
+		}
 	}
 
-	public function destroy(string $id)
+	public function destroy(Category $category)
 	{
-		//
+		try {
+			$this->authorize('delete', Category::class);
+			$category->delete();
+			return HttpResponse::success(message: 'Categoria excluída com sucesso');
+		} catch (\Throwable $th) {
+			if ($th instanceof AuthorizationException) {
+				return HttpResponse::error(message: 'Não tem permissão para excluir categoria');
+			}
+			return HttpResponse::error(message: 'Erro ao excluir categoria' . $th->getMessage());
+		}
 	}
 }
