bdc56b1302cab7f42393fd04a52a51c236d731a0
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-15 09:53:20 +0100

    refactor: Change storage from localStorage to sessionStorage

--- resources/app/infra/cache/local-storage-adapter.ts
+++ resources/app/infra/cache/local-storage-adapter.ts
@@ -2,14 +2,14 @@ import { IStorage } from '@/data/protocols/cache'
 
 export class LocalStorageAdapter implements IStorage {
 	set(key: string, value: string): void {
-		localStorage.setItem(key, value)
+		sessionStorage.setItem(key, value)
 	}
 
 	get(key: string): string {
-		return localStorage.getItem(key) as string
+		return sessionStorage.getItem(key) as string
 	}
 
 	remove(key: string): void {
-		localStorage.removeItem(key)
+		sessionStorage.removeItem(key)
 	}
 }
