d665c7e3385381e99a4ac97eaf0f339f55f0e2a2
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-29 21:38:53 +0100

    chore: add laravel-dompdf

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Sale;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class InvoiceController extends Controller
 {
 	public function sale(Sale $sale)
 	{
-		return view('invoices.sale', ['sale' => $sale]);
+		// return view('invoices.sale', ['sale' => $sale]);
+		Pdf::setOption(['defaultFont' => 'sans-serif']);
+		$pdf = Pdf::loadView('invoices.sale', ['sale' => $sale]);
+		return $pdf->stream("factura{$sale->id}.pdf");
 	}
 }
