3f802b92bf8c7440fc80caf4f2ff51dbf881dd89
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-10-30 09:46:22 +0000

    refactor: move pdf templates to pdfs folder

--- app/Http/Controllers/AdminDocsController.php
+++ app/Http/Controllers/AdminDocsController.php
@@ -10,16 +10,16 @@ class AdminDocsController extends Controller
 {
 	public function advertencia(User $employee)
 	{
-		// return view('admin-docs.advertencia', compact('employee'));
-		$pdf = Pdf::loadView('admin-docs.advertencia', compact('employee'));
+		// return view('pdfs.admin-docs.advertencia', compact('employee'));
+		$pdf = Pdf::loadView('pdfs.admin-docs.advertencia', compact('employee'));
 		$name = Str::slug($employee->name);
 		return	$pdf->stream("advertencia-{$employee->id}-{$name}.pdf");
 	}
 
 	public function autorizacaoConducao(User $employee)
 	{
-		// return view('admin-docs.advertencia', compact('employee'));
-		$pdf = Pdf::loadView('admin-docs.autorizacao-conducao', compact('employee'));
+		// return view('pdfs.admin-docs.advertencia', compact('employee'));
+		$pdf = Pdf::loadView('pdfs.admin-docs.autorizacao-conducao', compact('employee'));
 		$name = Str::slug($employee->name);
 		return	$pdf->stream("autorizacao-conducao-{$employee->id}-{$name}.pdf");
 	}
--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -9,9 +9,9 @@ class InvoiceController extends Controller
 {
 	public function sale(Sale $sale)
 	{
-		// return view('invoices.sale', ['sale' => $sale]);
+		// return view('pdfs.invoices.sale', ['sale' => $sale]);
 		Pdf::setOption(['defaultFont' => 'sans-serif']);
-		$pdf = Pdf::loadView('invoices.sale', ['sale' => $sale]);
+		$pdf = Pdf::loadView('pdfs.invoices.sale', ['sale' => $sale]);
 		return $pdf->stream("factura-{$sale->id}.pdf");
 	}
 }
