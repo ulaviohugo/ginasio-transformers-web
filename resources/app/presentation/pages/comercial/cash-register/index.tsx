0b4fd11433cf39a1caecf1fcc1c6cdbb6570c363
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-11-28 20:05:50 +0100

    style: add spacing

--- resources/app/presentation/pages/comercial/cash-register/index.tsx
+++ resources/app/presentation/pages/comercial/cash-register/index.tsx
@@ -77,103 +77,107 @@ export function CashRegister() {
 		<Layout>
 			{showGraph && <CashRegisterGraph onClose={() => setShowGraph(false)} />}
 			<LayoutBody>
-				<SubMenu submenus={MenuUtils.commercialMenuItens({ role: user.role })} />
-				<Title title="Caixa" icon={IconCashRegister} />
-				<CashRegisterEditor />
-				<fieldset>
-					<legend className="flex gap-1">
-						Filtro {isLoading && <Spinner />}
-						<ButtonSubmit
-							text="Ver gráfico"
-							showIcon={false}
-							onClick={() => setShowGraph(true)}
-						/>
-					</legend>
-					<div className="flex p-2 border">
-						<div className="flex gap-1 ml-auto items-end">
-							<Input
-								type="number"
-								name="year"
-								label="Ano"
-								value={filter?.year || ''}
-								onChange={handleChangeFilter}
+				<div className="flex flex-col gap-2">
+					<SubMenu submenus={MenuUtils.commercialMenuItens({ role: user.role })} />
+					<Title title="Caixa" icon={IconCashRegister} />
+					<CashRegisterEditor />
+					<fieldset>
+						<legend className="flex gap-1">
+							Filtro {isLoading && <Spinner />}
+							<ButtonSubmit
+								text="Ver gráfico"
+								showIcon={false}
+								onClick={() => setShowGraph(true)}
 							/>
-							<Select
-								name="month"
-								label="Mês"
-								value={filter?.month || ''}
-								data={DateUtils.getMonthList().map((month) => ({
-									text: DateUtils.getMonthExt(month),
-									value: String(month)
-								}))}
-								defaultText="Todos"
-								onChange={handleChangeFilter}
-							/>
-							<Select
-								name="operation_type"
-								label="Tipo de operação"
-								value={filter?.operation_type || ''}
-								data={['Entrada', 'Saída'].map((payment) => ({
-									text: payment
-								}))}
-								defaultText="Todos"
-								onChange={handleChangeFilter}
-							/>
-							<Select
-								name="payment_method"
-								label="Tipo de pagamento"
-								value={filter?.payment_method || ''}
-								data={PaymentUtils.getMethods().map((payment) => ({
-									text: payment
-								}))}
-								defaultText="Todos"
-								onChange={handleChangeFilter}
-							/>
-							<div className="flex">
-								<button
-									className="btn-primary"
-									onClick={handleFilter}
-									disabled={isLoading}
-								>
-									{isLoading ? <Spinner /> : <IconSearch />}
-								</button>
+						</legend>
+						<div className="flex p-2 border">
+							<div className="flex gap-1 ml-auto items-end">
+								<Input
+									type="number"
+									name="year"
+									label="Ano"
+									value={filter?.year || ''}
+									onChange={handleChangeFilter}
+								/>
+								<Select
+									name="month"
+									label="Mês"
+									value={filter?.month || ''}
+									data={DateUtils.getMonthList().map((month) => ({
+										text: DateUtils.getMonthExt(month),
+										value: String(month)
+									}))}
+									defaultText="Todos"
+									onChange={handleChangeFilter}
+								/>
+								<Select
+									name="operation_type"
+									label="Tipo de operação"
+									value={filter?.operation_type || ''}
+									data={['Entrada', 'Saída'].map((payment) => ({
+										text: payment
+									}))}
+									defaultText="Todos"
+									onChange={handleChangeFilter}
+								/>
+								<Select
+									name="payment_method"
+									label="Tipo de pagamento"
+									value={filter?.payment_method || ''}
+									data={PaymentUtils.getMethods().map((payment) => ({
+										text: payment
+									}))}
+									defaultText="Todos"
+									onChange={handleChangeFilter}
+								/>
+								<div className="flex">
+									<button
+										className="btn-primary"
+										onClick={handleFilter}
+										disabled={isLoading}
+									>
+										{isLoading ? <Spinner /> : <IconSearch />}
+									</button>
+								</div>
 							</div>
 						</div>
-					</div>
-					<table className="w-full text-sm">
-						<thead className="font-semibold uppercase border-b">
-							<tr>
-								<td>Data</td>
-								<td>Descrição de operação</td>
-								<td>Tipo operação</td>
-								<td>Valor</td>
-								<td>Saldo após movimento</td>
-								<td>Movimento bancário</td>
-							</tr>
-						</thead>
-						<tbody>
-							{transactions.map((transaction, i) => {
-								const bg = i % 2 == 0 ? 'bg-gray-50' : 'bg-none'
-								const sinal = transaction.operation_type == 'Entrada' ? '+' : '-'
-								return (
-									<tr key={transaction.id} className={`${bg} hover:bg-gray-100`}>
-										<td>{DateUtils.getDatePt(transaction.date)}</td>
-										<td>{transaction.description}</td>
-										<td>{transaction.operation_type}</td>
-										<td className={`${sinal == '+' ? 'text-green-600' : 'text-red-500'}`}>
-											{sinal}
-											{NumberUtils.formatCurrency(transaction.amount)}
-										</td>
-										<td>
-											{NumberUtils.formatCurrency(transaction.post_movement_balance)}
-										</td>
-										<td>{transaction.payment_method}</td>
-									</tr>
-								)
-							})}
-						</tbody>
-					</table>
-				</fieldset>
+						<table className="w-full text-sm">
+							<thead className="font-semibold uppercase border-b">
+								<tr>
+									<td>Data</td>
+									<td>Descrição de operação</td>
+									<td>Tipo operação</td>
+									<td>Valor</td>
+									<td>Saldo após movimento</td>
+									<td>Movimento bancário</td>
+								</tr>
+							</thead>
+							<tbody>
+								{transactions.map((transaction, i) => {
+									const bg = i % 2 == 0 ? 'bg-gray-50' : 'bg-none'
+									const sinal = transaction.operation_type == 'Entrada' ? '+' : '-'
+									return (
+										<tr key={transaction.id} className={`${bg} hover:bg-gray-100`}>
+											<td>{DateUtils.getDatePt(transaction.date)}</td>
+											<td>{transaction.description}</td>
+											<td>{transaction.operation_type}</td>
+											<td
+												className={`${sinal == '+' ? 'text-green-600' : 'text-red-500'}`}
+											>
+												{sinal}
+												{NumberUtils.formatCurrency(transaction.amount)}
+											</td>
+											<td>
+												{NumberUtils.formatCurrency(transaction.post_movement_balance)}
+											</td>
+											<td>{transaction.payment_method}</td>
+										</tr>
+									)
+								})}
+							</tbody>
+						</table>
+					</fieldset>
+				</div>
 			</LayoutBody>
 		</Layout>
 	)
