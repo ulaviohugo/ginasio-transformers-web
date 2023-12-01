64b04af8c3927ed04c6ff3d8ebd2e82c458d6e9f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-12-01 08:10:57 +0100

    feat: add helper methods to FileHelper

--- app/Helpers/FileHelper.php
+++ app/Helpers/FileHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Helpers;
 
+use Exception;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -10,7 +11,6 @@ class FileHelper
 	static function uploadBase64(string $base64, $path = '')
 	{
 		$extension = explode('/', explode(':', substr($base64, 0, strpos($base64, ';')))[1])[1];   // .jpg .png .pdf
-
 		$replace = substr($base64, 0, strpos($base64, ',') + 1);
 
 		// find substring fro replace here eg: data:image/png;base64,
@@ -24,7 +24,7 @@ class FileHelper
 
 	static function storagePath($path = '')
 	{
-		return storage_path($path);
+		return public_path('/storage/' . $path);
 	}
 
 	static function storageLink($file = '')
@@ -47,5 +47,43 @@ class FileHelper
 		} catch (\Throwable $th) {
 			return false;
 		}
+	}
+
+	public static function convertToBase64(string $filePath)
+	{
+		try {
+			if (!file_exists($filePath)) {
+				throw new Exception("Arquivo não encontrado: $filePath");
+			}
+
+			$fileContent = file_get_contents($filePath);
+
+			if ($fileContent === false) {
+				throw new Exception("Erro ao ler o conteúdo do arquivo: $filePath");
+			}
+
+			$fileInfo = pathinfo($filePath);
+			$fileExtension = $fileInfo['extension'];
+
+			$fileBase64 = base64_encode($fileContent);
+
+			if ($fileBase64 === false) {
+				throw new Exception("Erro ao codificar o conteúdo em base64: $filePath");
+			}
+
+			return "data:$fileExtension;base64,$fileBase64";
+		} catch (Exception $e) {
+			return "Erro ao converter o arquivo para base64: " . $e->getMessage();
+		}
+	}
+
+	public static function logoPath()
+	{
+		return public_path('/images/logo.png');
+	}
+
+	public static function logoUrl()
+	{
+		return asset('/images/logo.png');
 	}
 }
