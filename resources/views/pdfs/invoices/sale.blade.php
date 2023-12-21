f7f1f6bebb6055fdd1715ccc9b65b009b4bbe828
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-12-21 21:54:35 +0100

    style: change sale invoice style

--- resources/views/pdfs/invoices/sale.blade.php
+++ resources/views/pdfs/invoices/sale.blade.php
@@ -21,20 +21,17 @@ $logo = $fileHelper::convertToBase64($logoPath);
 		}
 
 		* {
-			font-family: 'Calibri (Corpo)', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+			font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
 		}
 
 		body {
-			padding: 8px
+			font-size: 11px;
 		}
 
 		.page {
 			max-width: 800px;
 			margin: auto;
-			/* padding: 48px; */
-			border: 1px solid #666;
-			font-family: "Century Gothic";
-			font-size: 12px
+			padding: 0 48px;
 		}
 
 		table {
@@ -45,13 +42,16 @@ $logo = $fileHelper::convertToBase64($logoPath);
 			padding: 8px
 		}
 
-		td.border {
-			border-left: 1px solid #666;
-			border-bottom: 1px solid #666;
+		.border {
+			border: 1px solid #ddd
 		}
 
-		td.border:first-child {
-			border-left: none
+		.border-b {
+			border-bottom: 1px solid #000
+		}
+
+		.bold {
+			font-weight: bold
 		}
 	</style>
 </head>
@@ -61,19 +61,24 @@ $logo = $fileHelper::convertToBase64($logoPath);
 	ob_start();
 	?>
 	<div class="page">
-		<table cellspacing="0">
-			<tr>
-				<td colspan="3" style="background: #404040; height:50px"><img src="{{$logo}}" style="width: 100px" alt=""></td>
-			</tr>
-			<tr>
-				<td><b>Nº Factura:</b> <u>{{$sale->id}}</u></td>
-				<td style="text-align: right">
-					<b>Data</b> <u>{{date('d/m/Y', strtotime($sale->created_at))}}</u>
-				</td>
-				<td style="text-align: right">
-					<b>Hora</b> <u>{{date('H:i:s', strtotime($sale->created_at))}}</u>
-				</td>
-			</tr>
+		<div class="border" style="font-size:14px; margin-bottom: 8px">
+			<table>
+				<tr>
+					<td><img src="{{$logo}}" style="width: 50px; margin: 4px 0" alt=""></td>
+					<td style="width: 100px">NIF: XXXXXX</td>
+					<td style="width: 100%">
+						{{env('ADDRESS')}}
+					</td>
+					<td style="width: 100px">
+						<div>CLIENTE</div>
+						<div>
+							<b>Nº Factura:</b> <u>{{$sale->id}}</u>
+						</div>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<table class="border" style="margin-bottom: 8px">
 			<tr>
 				<td>
 					<b>
@@ -85,28 +90,36 @@ $logo = $fileHelper::convertToBase64($logoPath);
 				<td style="text-align: right">
 					<b>Antendente</b> <u>{{$sale->employee?->name ?? $sale->user?->name}}</u>
 				</td>
+				<td style="text-align: right">
+					<b>Data</b> <u>{{date('d/m/Y', strtotime($sale->created_at))}}
+						{{date('H:i:s', strtotime($sale->created_at))}}</u>
+				</td>
 			</tr>
 		</table>
 
 		<table cellspacing="0">
-			<tr style="font-weight: bold; background: #404040; color: #fff">
-				<td class="border">PRODUTO</td>
-				<td class="border">PREÇO</td>
-				<td class="border">QUANTIDADE</td>
-				<td class="border">DESCONTO</td>
-				<td class="border">VALOR TOTAL</td>
+			<tr>
+				<td class="border bold" style="width: 90px">CÓDIGO BARRA</td>
+				<td class="border bold">PRODUTO</td>
+				<td class="border bold" style="width: 80px">PREÇO</td>
+				<td class="border bold" style="width: 80px">QUANTIDADE</td>
+				<td class="border bold" style="width: 80px">DESCONTO</td>
+				<td class="border bold" style="width: 80px">VALOR TOTAL</td>
 			</tr>
 			@foreach ($sale->productSales as $productSale)
 			<tr>
+				<td class="border">{{$productSale->product->id}}</td>
 				<td class="border">{{$productSale->product->name}}</td>
 				<td class="border">{{App\Helpers\NumberHelper::formatCurrency($productSale->unit_price)}}</td>
 				<td class="border">{{$productSale->quantity}}</td>
-				<td class="border">{{$productSale->discount ? App\Helpers\NumberHelper::formatCurrency($productSale->discount) :
+				<td class="border">{{$productSale->discount ? App\Helpers\NumberHelper::formatCurrency($productSale->discount)
+					:
 					'0,00'}}</td>
 				<td class="border">{{App\Helpers\NumberHelper::formatCurrency($productSale->total_value)}}</td>
 			</tr>
 			@endforeach
 			<tr style="font-weight: bold">
+				<td></td>
 				<td></td>
 				<td></td>
 				<td></td>
@@ -117,6 +130,7 @@ $logo = $fileHelper::convertToBase64($logoPath);
 				<td></td>
 				<td></td>
 				<td></td>
+				<td></td>
 				<td class="border">IVA 14%</td>
 				<td class="border">0,00</td>
 			</tr>
@@ -124,15 +138,17 @@ $logo = $fileHelper::convertToBase64($logoPath);
 				<td></td>
 				<td></td>
 				<td></td>
+				<td></td>
 				<td class="border">TOTAL A PAGAR</td>
 				<td class="border">{{App\Helpers\NumberHelper::formatCurrency($sale->amount_paid)}}</td>
 			</tr>
 		</table>
-		<div style="font-weight: bold; padding: 8px; text-align: center">
+		<div style="padding: 8px; text-align: center; margin-top: 8px">
 			<div style="padding-bottom: 5px">Muito obrigado. Volte sempre!</div>
 			<div style="padding-bottom: 5px">A sua preferência nos impulsiona a servir com rigor e qualidade.</div>
-			<div style="padding-bottom: 5px; color: rgb(192,0,0)">IVA 14% - Regime de não sujeição - Regime Simplificado</div>
-			<div style="padding-bottom: 5px">Tel. +244 923 465 361 / +244 990 912 842</div>
+			<div style="padding-bottom: 5px; color: rgb(192,0,0)">IVA 14% - Regime de não sujeição - Regime Simplificado
+			</div>
+			<div style="padding-bottom: 5px">Tel. +244 923 824 204</div>
 			<div style="padding-bottom: 5px">Sistema desenvolvido por Samuel Freitas - +244 930 690 710</div>
 		</div>
 	</div>
@@ -140,9 +156,21 @@ $logo = $fileHelper::convertToBase64($logoPath);
 		$invoice = ob_get_clean();
 	?>
 
-	{!!$invoice!!}
+	<br />
+	<br />
 	<br />
 	{!!$invoice!!}
+	<br />
+	<div style="color: #888; border: 0; padding: 0; font-size: 9px; text-align: center">
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		- - - - - - - - - -
+		corte aqui
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+		- - - - - - - - - -
+	</div>
+	<br />
+	<br />
+	{!!str_replace('CLIENTE','WO',$invoice)!!}
 </body>
 
 </html>
