4cb6cfcc6433ad91800774332dbfc89410d517d1
Ulávio Hugo <ulaviohugo@gmail.com>
2024-02-06 21:00:56 +0100

    refactor: change fivicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
 <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" href="/images/logo.png" type="image/x-icon" sizes="244x256"/><meta name="next-size-adjust"/>
 	<title></title>
 	@viteReactRefresh
 @vite('resources/app/main/app.tsx')
